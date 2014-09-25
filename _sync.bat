ac56a20747338d3a75fbe1cbe58cdc10313a6bc2
FriendlyWP <michelle@friendlywebconsulting.com>
2014-09-25 10:13:53 -0500

    Initial rev

--- _sync.bat
+++ _sync.bat
@@ -1,2 +1,2 @@
-cd "C:\Users\Michelle\Documents\Websites\master.dev\wp-content\themes\bones\library\scss"
+cd "C:\Users\Michelle\Documents\Websites\kidlit.dev\wp-content\themes\kidlittv-bones\library\scss"
 compass watch
