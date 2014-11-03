d07145ace4aca8cd633649a177c710db20695c2e
FriendlyWP <michelle@friendlywebconsulting.com>
2014-11-03 14:40:51 -0600

    Chad updates
    Pre-banner changes

--- single.php
+++ single.php
@@ -30,6 +30,35 @@
 
 						<?php endif; ?>
 
+						<div class="postnav cf">
+							<?php $prev_post = get_previous_post(TRUE);
+								if (!empty( $prev_post )) { ?>
+								<div class="prevpost">
+									<?php if (has_post_thumbnail($prev_post->ID) ) { ?>
+										<a class="thumbimg" href="<?php echo get_permalink( $prev_post->ID ); ?>"><?php echo get_the_post_thumbnail($prev_post->ID, 'tiny-thumb'); ?></a>
+										<span>
+											<h6>Previous</h6>
+											<a href="<?php echo get_permalink( $prev_post->ID ); ?>"><?php echo $prev_post->post_title; ?></a>
+										</span>
+									<?php } ?>
+ 								</div>
+ 							<?php } ?>
+
+							<?php $next_post = get_next_post(TRUE);
+								if (!empty( $next_post )) { ?>
+								<div class="nextpost">
+									<?php if (has_post_thumbnail($next_post->ID) ) { ?>
+										<a class="thumbimg" href="<?php echo get_permalink( $next_post->ID ); ?>"><?php echo get_the_post_thumbnail($next_post->ID, 'tiny-thumb'); ?></a>
+										<span>
+											<h6>Next</h6>
+											<a href="<?php echo get_permalink( $next_post->ID ); ?>"><?php echo $next_post->post_title; ?></a>
+										</span>
+									<?php } ?>
+ 								</div>
+ 							<?php } ?>
+							
+						</div><!-- .postnav -->
+
 					</div>
 
 					<?php get_sidebar(); ?>
